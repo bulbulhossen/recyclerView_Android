9c34af531e328e4b2f605365e0c6ebbbb8cd06a7
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-02-13 22:57:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/materialstyleddialogs_android/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.appsd.materialstyleddialogs_android;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> countries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        countries = new ArrayList<>();
+        countries.add("ULAB");
+        countries.add("IUBT");
+        countries.add("DU");
+        countries.add("UIU");
+        countries.add("SAST");
+        RecyclerView.Adapter adapter = new DataAdapter(countries);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                if (e.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                    View child = rv.findChildViewUnder(e.getX(), e.getY());
+                    int position = rv.getChildAdapterPosition(child);
+                    Toast.makeText(getApplicationContext(), countries.get(position), Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+}
